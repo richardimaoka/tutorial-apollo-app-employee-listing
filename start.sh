fa6ecdc03b567d39ba30ba127f246568bb1bdd1f
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-10 22:02:11 +0900

    ./start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-(cd file-server && npm run file-server-start &)
+(cd answers/step-final/file-server && npm run file-server-start &)
 FILE_SERVER_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
 
-(cd server && npm run server-start &)
+(cd answers/step-final/server && npm run server-start &)
 SERVER_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
 
-(cd server && npm run server-generate &)
+(cd answers/step-final/server && npm run server-generate &)
 SERVER_GENERATE_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$SERVER_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
 
-(cd client && npm run client-start &)
+(cd answers/step-final/client && npm run client-start &)
 CLIENT_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
 
-(cd client && npm run client-generate &)
+(cd answers/step-final/client && npm run client-generate &)
 CLIENT_GENERATE_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$CLIENT_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
 

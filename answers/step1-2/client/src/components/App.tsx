3054f73eca66cbef2dff82cf00d98f2d76c87edc
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-12 08:22:28 +0900

    ApolloClient initialization

--- answers/step1-2/client/src/components/App.tsx
+++ answers/step1-2/client/src/components/App.tsx
@@ -1,4 +1,10 @@
 import { HeaderContainer } from "./header/HeaderContainer";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (

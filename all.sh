659153d3ebcbd1c8d7540473af40846780a4c6a5
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-13 22:27:56 +0900

    update all.sh

--- all.sh
+++ all.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 scripts/cleanup.sh
+trap scripts/cleanp.sh 
 
 ###############################
 # let's start the steps
 ###############################
-steps/step1-1-client.sh
-(cd steps && ./step1-1-client.sh)
+steps/step1-1-client.sh 1>/dev/null &
 REACT_CLIENT_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$REACT_CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
+steps/step1-1.test.sh
 
 # ###############################
 # # finished
@@ -17,4 +18,4 @@ trap 'scripts/kill-all-child-pids.sh "$REACT_CLIENT_PID"' 2 # 2 means SIGINT = C
 # scripts/kill-all-child-pids.sh "$CLIENT_PID"
 # scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"
 
-scripts/cleanup.sh
+# scripts/cleanup.sh
--- steps/step1-1-client.sh
+++ steps/step1-1-client.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh 
+cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
+
 # ## 1. React環境のセットアップ
 
 # :large_orange_diamond: Action: 以下のコマンドを実行してください

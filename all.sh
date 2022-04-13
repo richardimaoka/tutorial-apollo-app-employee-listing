396fc37929429045d0e67fcb79ee008f785b3dc2
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-13 23:16:39 +0900

    no trap needed?

--- all.sh
+++ all.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 scripts/cleanup.sh
-trap scripts/cleanp.sh 
 
 ###############################
 # let's start the steps
 ###############################
 steps/step1-1-client.sh 1>/dev/null &
 REACT_CLIENT_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$REACT_CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
+echo "react client pid = $REACT_CLIENT_PID"
+# trap 'scripts/kill-all-child-pids.sh "$REACT_CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
 steps/step1-1.test.sh
+sleep 60
 
 # ###############################
 # # finished
 # ###############################
 
-# scripts/kill-all-child-pids.sh "$CLIENT_PID"
+# scripts/kill-all-child-pids.sh "$REACT_CLIENT_PID"
 # scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"
 
 # scripts/cleanup.sh
--- start.sh
+++ start.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 (cd answers/step-final/file-server && npm run file-server-start &)
-FILE_SERVER_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
+# FILE_SERVER_PID="$!"
+# trap 'scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
 
 (cd answers/step-final/server && npm run server-start &)
-SERVER_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
+# SERVER_PID="$!"
+# trap 'scripts/kill-all-child-pids.sh "$SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
 
 (cd answers/step-final/server && npm run server-generate &)
-SERVER_GENERATE_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$SERVER_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
+# SERVER_GENERATE_PID="$!"
+# trap 'scripts/kill-all-child-pids.sh "$SERVER_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
 
 (cd answers/step-final/client && npm run client-start &)
-CLIENT_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
+# CLIENT_PID="$!"
+# trap 'scripts/kill-all-child-pids.sh "$CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
 
 (cd answers/step-final/client && npm run client-generate &)
-CLIENT_GENERATE_PID="$!"
-trap 'scripts/kill-all-child-pids.sh "$CLIENT_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
+# CLIENT_GENERATE_PID="$!"
+# trap 'scripts/kill-all-child-pids.sh "$CLIENT_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
 
 # wait infinitely
 tail -f /dev/null

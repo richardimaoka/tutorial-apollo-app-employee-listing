bc48b2efc8b69d1b4ecba4b3d335092efa96e328
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-15 04:02:33 +0900

    update graphql test

--- steps/1-2.test.sh
+++ steps/1-2.test.sh
@@ -3,24 +3,28 @@
 cd "$(dirname "$0")" || exit
 cd ../ || exit # cd to the git repository root
 
+EXIT_STATUS=0
 CURRENT_FILE=$(basename "$0")
 
-if scripts/keep-curling-graphql.sh http://localhost:4000
+if ! scripts/keep-curling-graphql.sh http://localhost:4000
 then
-  echo "SUCCESS ($CURRENT_FILE): GraphQL server working" 1>&2
-  exit 0
-else
   echo "FAILED ($CURRENT_FILE): GraphQL server not working" 1>&2
-  exit 1
+  EXIT_STATUS=1
 fi
 
 GRAPHQL_QUERY_ONE_LINE=$(sed -z 's/\n//g' server/query.gql)
-GRAPHQL_RESULT=$(curl -X POST -H "Content-Type: application/json" -d "{ \"query\": \"$GRAPHQL_QUERY_ONE_LINE\"}" http://localhost:4000)
-if echo "$GRAPHQL_RESULT" | jq
+GRAPHQL_RESULT=$(curl -X POST -H "Content-Type: application/json" -d "{ \"query\": \"$GRAPHQL_QUERY_ONE_LINE\"}" -s http://localhost:4000)
+if ! echo "$GRAPHQL_RESULT" | jq 1>/dev/null
 then
-  echo "SUCCESS ($CURRENT_FILE): GraphQL query retured a valid response" 1>&2
-  exit 0
-else
   echo "FAILED ($CURRENT_FILE): GraphQL query did not receive a valid response" 1>&2
-  exit 1
+  echo "$GRAPHQL_RESULT" 1>&2
+  EXIT_STATUS=1
 fi
+
+if [ $EXIT_STATUS -eq "0" ]
+then
+  echo "SUCCESS ($CURRENT_FILE): test successful"
+fi 
+
+# if either is missing, then non-zero exit
+exit $EXIT_STATUS

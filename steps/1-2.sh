a9f45d455a7d29966140763576dce1f0bb95b3ba
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-17 05:02:53 +0900

    updated step1-2

--- steps/1-2.sh
+++ steps/1-2.sh
@@ -3,13 +3,13 @@
 cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh 
 cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
 
+# :large_orange_diamond: Action: ctrl+cでクライアント・ターミナルを停止してください
+
 # :large_orange_diamond: Action: 以下のコマンドを実行してください
 
 # ```terminal: クライアント
-npm install @apollo/client graphql @graphql-cli/codegen
-# ```
-
-# ```terminal: クライアント
-cp answers/step1-3/client/src/components/App.tsx client/src/components/App.tsx 
+rm -rf client/src
+rm -rf client/public
+git apply patches/1-2.patch
 # ```
 

a9521ba5ffbebe050ff6c15d1b73b258d51bbe13
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-04-17 02:54:50 +0900

    s: init client in answer

--- steps/1-1-client.sh
+++ steps/1-1-client.sh
@@ -12,19 +12,8 @@ npx --yes create-react-app client --template typescript
 # ```
 
 # ```terminal: クライアント
-rm -rf client/public
-rm -rf client/src   
-cp -r  answers/step1-1/client/src client/src
-cp -r  answers/step1-1/client/public client/public
-# ```
-
-# ```terminal: クライアント
 # shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh 
 cd client
-npm install
-# ```
-
-# ```terminal: クライアント
 npm set-script client-start "npm run start"
 npm run client-start
 # ```

83e25d3209077972305aac319e84859247306f4e
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-03 21:25:37 +0900

    barebone server initial files in txt

--- create-patches.sh
+++ create-patches.sh
@@ -2,3 +2,6 @@
 
 git diff --binary --relative=answer step1-2-1 step1-2-2 answer > patches/1-2.patch
 git diff --binary --relative=answer step1-3 step1-4 answer > patches/1-4.patch
+
+
+git diff --relative=answer 2df5eda 1971b8b > patches/202253-1-1.patch

c1d01ed72d4efc182266b16616e0c1a77937c43d
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-03 21:53:29 +0900

    s: start.sh

---
+++ answer/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+(cd server && npm run server-start &)
+SERVER_PID="$!"
+trap 'scripts/kill-all-child-pids.sh "$SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
+
+(cd client && npm run client-start &)
+CLIENT_PID="$!"
+trap 'scripts/kill-all-child-pids.sh "$CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
+
+(cd file-server && npm run file-server-start &)
+FILE_SERVER_PID="$!"
+trap 'scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c
+
+# wait infinitely
+tail -f /dev/null

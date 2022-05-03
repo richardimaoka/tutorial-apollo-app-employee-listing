722ae6b8f7320f4ab9bf99da974cac7dad1a9ef6
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-04 06:44:05 +0900

    s: setup client generate

--- answer/start.sh
+++ answer/start.sh
@@ -12,6 +12,10 @@ trap 'scripts/kill-all-child-pids.sh "$SERVER_GENERATE_PID"' 2 # 2 means SIGINT 
 CLIENT_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$CLIENT_PID"' 2 # 2 means SIGINT = Ctrl + c
 
+(cd client && npm run client-generate &)
+CLIENT_GENERATE_PID="$!"
+trap 'scripts/kill-all-child-pids.sh "$CLIENT_GENERATE_PID"' 2 # 2 means SIGINT = Ctrl + c
+
 (cd file-server && npm run file-server-start &)
 FILE_SERVER_PID="$!"
 trap 'scripts/kill-all-child-pids.sh "$FILE_SERVER_PID"' 2 # 2 means SIGINT = Ctrl + c

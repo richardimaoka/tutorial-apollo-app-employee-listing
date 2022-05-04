7be959b9ad1d9298b9af31fcf11c27f411cfdbbe
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-04 10:57:47 +0900

    step1-1

--- create-patches.sh
+++ create-patches.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+ln -s ../patches patches
+
 git diff --binary --relative=answer step1-2-1 step1-2-2 answer > patches/1-2.patch
 git diff --binary --relative=answer step1-3 step1-4 answer > patches/1-4.patch
 
 
 git diff --relative=answer 2df5eda 1971b8b > patches/202253-1-1.patch
+
+
+
+git show --relative=answer --patch 1971b8b > patches/1-1.patch 
+git apply patches/1-1.patch 
+
+# ↓.gitignoreされたディレクトリ内など、gitレポジトリの外ではgit applyがすべてを無視する
+patch -p1 < patches/1-1.patch 
--- steps/1-1-client.sh
+++ steps/1-1-client.sh
@@ -3,18 +3,53 @@
 cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh 
 cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
 
-# ## 1. React環境のセットアップ
+### 開発プロジェクトの初期設定
 
-# :large_orange_diamond: Action: 以下のコマンドを実行してください
+## GraphQL サーバー側 セットアップ
 
-# ```terminal: クライアント
-npx --yes create-react-app client --template typescript 
-# ```
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
-# ```terminal: クライアント
+# ```terminal: メイン
 # shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh 
-cd client
-npm set-script client-start "npm run start"
-npm run client-start
+cd "$(git rev-parse --show-toplevel)" # gitレポジトリのルートディレクトリに移動
+mkdir server
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh 
+cd server
+echo "node_modules" >> .gitignore
 # ```
 
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+npm init -yes
+npm install apollo-server graphql
+npm install --save-dev typescript ts-node-dev 
+npx tsc -init
+# ```
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+cd ../
+git apply patches/1-1.patch
+# ```
+
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh 
+cd server
+npm set-script server-start "ts-node-dev --watch schema.gql,data.json --respawn index.ts"
+npm run server-start
+# ```
+
+# :large_orange_diamond: Action: 以下のクエリをApollo Studio Explorerで入力してRunを押してください。
+
+# ```terminal: メイン
+{
+  hello
+}
+# ```
+
+# :large_orange_diamond: Action: Ctrl+C

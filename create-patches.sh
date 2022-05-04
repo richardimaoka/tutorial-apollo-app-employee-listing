cfa9c0c4b540824b6302d385cdd75ce6759b4695
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-04 15:22:46 +0900

    step1-1

--- create-patches.sh
+++ create-patches.sh
@@ -9,9 +9,12 @@ git diff --binary --relative=answer step1-3 step1-4 answer > patches/1-4.patch
 git diff --relative=answer 2df5eda 1971b8b > patches/202253-1-1.patch
 
 
-
+# index.ts data.json schema.graphql
 git show --relative=answer --patch 1971b8b > patches/1-1.patch 
 git apply patches/1-1.patch 
 
 # ↓.gitignoreされたディレクトリ内など、gitレポジトリの外ではgit applyがすべてを無視する
 patch -p1 < patches/1-1.patch 
+
+# start.sh
+git show --relative=answer --patch 6fa56ca > patches/1-2.patch
--- steps/1-1.sh
+++ steps/1-1.sh
@@ -30,7 +30,7 @@ npx tsc -init
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal: メイン
-cd ../
+cd ../ # gitレポジトリのルートディレクトリに移動
 git apply patches/1-1.patch
 # ```
 
@@ -38,18 +38,70 @@ git apply patches/1-1.patch
 # :large_orange_diamond: Action: 以下のコマンドを入力してください。
 
 # ```terminal: メイン
-# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh 
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
 cd server
 npm set-script server-start "ts-node-dev --watch schema.gql,data.json --respawn index.ts"
-npm run server-start
+# npm run server-start
 # ```
 
 # :large_orange_diamond: Action: 以下のクエリをApollo Studio Explorerで入力してRunを押してください。
 
 # ```terminal: メイン
-{
-  hello
-}
+# {
+#   hello
+# }
+# ```
+
+# :large_orange_diamond: Action: Ctrl+C
+
+## React クライアント側 セットアップ
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+cd ../ # gitレポジトリのルートディレクトリに移動
+npx --yes create-react-app client --template typescript
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
+cd client 
+npm install @apollo/client graphql
+npm set-script client-start "react-scripts start"
+# ```
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+# npm run client-start
+# ```
+
+# https://github.com/apollographql/invariant-packages/issues/279
+# WARNING in ./node_modules/ts-invariant/lib/invariant.js
+
+# :large_orange_diamond: Action: Ctrl+C
+
+## file-server 導入
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+cd ../ # " # gitレポジトリのルートディレクトリに移動
+
+mkdir file-server
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
+cd file-server
+echo "node_modules" >> .gitignore
+
+npm init -yes
+npm install http-server
+npm set-script http-server-start "http-server"
+# ```
+
+# :large_orange_diamond: Action: 以下のコマンドを入力してください。
+
+# ```terminal: メイン
+cd ../ # " # gitレポジトリのルートディレクトリに移動
+git apply patches/1-1.patch
+chmod +x start.sh
+./start.sh
 # ```
 
 # :large_orange_diamond: Action: Ctrl+C
